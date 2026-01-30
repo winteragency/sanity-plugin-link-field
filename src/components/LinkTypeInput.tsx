552db34ba093f814310caafbef3a76139c7dbbe0
Frode Stenstrøm <frode.stenstrom@prosesspilotene.no>
2026-01-30 12:14:16 +0100

    refactor: replace styled-components with inline styles
    - Remove styled-components dependency usage from components
    - Use inline styles and style props instead
    - Refactor LinkTypeInput icon sizing with wrapper components

--- src/components/LinkTypeInput.tsx
+++ src/components/LinkTypeInput.tsx
@@ -1,11 +1,12 @@
 import {ChevronDownIcon} from '@sanity/icons'
 import {Button, Menu, MenuButton, MenuItem} from '@sanity/ui'
-import {AtSignIcon, GlobeIcon, LinkIcon, PhoneIcon} from 'lucide-react'
+import {AtSignIcon, GlobeIcon, LinkIcon, PhoneIcon, type LucideIcon} from 'lucide-react'
+import {type ComponentType, memo} from 'react'
 import {set, type StringInputProps} from 'sanity'
-import styled from 'styled-components'
 
 import {CustomLinkType, LinkFieldPluginOptions, LinkType} from '../types'
-import {memo} from 'react'
+
+const ICON_SIZE = 16
 
 const defaultLinkTypes: LinkType[] = [
   {title: 'Internal', value: 'internal', icon: LinkIcon},
@@ -14,21 +15,33 @@ const defaultLinkTypes: LinkType[] = [
   {title: 'Phone', value: 'phone', icon: PhoneIcon},
 ]
 
-const LinkTypeButton = styled(Button)`
-  height: 35px;
+/**
+ * Check if the icon is a lucide icon (one of the default link types)
+ */
+function isLucideIcon(icon: ComponentType): boolean {
+  return defaultLinkTypes.some((t) => t.icon === icon)
+}
 
-  svg.lucide {
-    width: 1rem;
-    height: 1rem;
+/**
+ * Create a sized wrapper for lucide icons
+ */
+function createSizedIcon(Icon: LucideIcon): ComponentType {
+  function SizedIcon() {
+    return <Icon size={ICON_SIZE} />
   }
-`
+  SizedIcon.displayName = `SizedIcon(${Icon.displayName || Icon.name || 'Unknown'})`
+  return SizedIcon
+}
 
-const LinkTypeMenuItem = styled(MenuItem)`
-  svg.lucide {
-    width: 1rem;
-    height: 1rem;
+/**
+ * Get the icon component for a link type, wrapping lucide icons to set the correct size
+ */
+function getIcon(type: LinkType): ComponentType {
+  if (isLucideIcon(type.icon)) {
+    return createSizedIcon(type.icon as LucideIcon)
   }
-`
+  return type.icon
+}
 
 /**
  * Custom input component for the "type" field on the link object.
@@ -56,23 +69,24 @@ export const LinkTypeInput = memo(function LinkTypeInput({
   return (
     <MenuButton
       button={
-        <LinkTypeButton
+        <Button
           type="button"
           mode="ghost"
-          icon={selectedType.icon}
+          icon={getIcon(selectedType)}
           iconRight={ChevronDownIcon}
           title="Select link type"
           aria-label={`Select link type (currently: ${selectedType.title})`}
+          style={{height: '35px'}}
         />
       }
       id="link-type"
       menu={
         <Menu>
           {linkTypes.map((type) => (
-            <LinkTypeMenuItem
+            <MenuItem
               key={type.value}
               text={type.title}
-              icon={type.icon}
+              icon={getIcon(type)}
               onClick={() => {
                 onChange(set(type.value))
               }}
